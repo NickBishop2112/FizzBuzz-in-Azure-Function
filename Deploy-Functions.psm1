5a62a4c49cabc50da747c527858fc671866dd21a
sunwado <30340714+sunwado@users.noreply.github.com>
2018-12-15 16:53:50 +0000

    Create Azure Resource Group

--- Deploy-Functions.psm1
+++ Deploy-Functions.psm1
@@ -5,3 +5,23 @@
 # Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
 # This improves performance of command discovery in PowerShell.
 Export-ModuleMember -Function *-*
+
+function New-AzureResourceGroup
+(
+    [Parameter(Mandatory=$true)][string]$name,
+    [Parameter(Mandatory=$true)][string]$location
+)
+{
+    $resourceGroup = (Get-AzureRmResourceGroup -Name $Name -Location $Location -ErrorAction Ignore)
+
+    if (!$resourceGroup)
+    {
+        New-AzureRmResourceGroup `
+            -Name $name `
+            -Location $location
+    }
+    else
+    {
+        Write-Warning "'$name' Resource Group located in '$location' already exists"
+    }
+}
