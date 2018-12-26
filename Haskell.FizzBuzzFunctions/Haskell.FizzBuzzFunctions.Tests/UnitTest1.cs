9cb4746debf3921194e3f9152c8304feb8af786b
nick <nickthebish@hotmail.com>
2018-12-26 19:23:59 +0000

    Create Azure Function Structure

---
+++ Haskell.FizzBuzzFunctions/Haskell.FizzBuzzFunctions.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Haskell.FizzBuzzFunctions.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
