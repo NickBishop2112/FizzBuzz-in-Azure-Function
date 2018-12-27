45417b8dae9546636b7459577ca047a2504ffa99
nick <nickthebish@hotmail.com>
2018-12-27 15:01:08 +0000

    Refactored logging

--- Haskell.FizzBuzzFunctions/Haskell.FizzBuzzFunctions.Tests/FizzBuzzGeneratorTests.cs
+++ Haskell.FizzBuzzFunctions/Haskell.FizzBuzzFunctions.Tests/FizzBuzzGeneratorTests.cs
@@ -23,16 +23,17 @@ namespace FizzBuzz.Application.Tests
         public void GivenGenerateWhenMultiplesOfThreeThenFizz()
         {
             var result = FizzBuzzGenerator.Generate(3, this.logger.Object);
-            
-           result.Should().Be("Fizz");
-           this.logger.Verify(x => x.Log(LogLevel.Information, It.IsAny<EventId>(), It.Is<FormattedLogValues>(y => y[0].Value.ToString() == "Item '3' is processed"), null, It.IsAny<Func<object, Exception, string>>()), Times.Once);
+
+            result.Should().Be("Fizz");
+            this.VerifyLogging("Item '3' is processed");
         }
-       
+
         [TestMethod]
         public void GivenGenerateWhenMultiplesOfFiveThenBuzz()
         {
             var result = FizzBuzzGenerator.Generate(5, this.logger.Object);
             result.Should().Be("Buzz");
+            this.VerifyLogging("Item '5' is processed");
         }
 
         [TestMethod]
@@ -40,6 +41,30 @@ namespace FizzBuzz.Application.Tests
         {
             var result = FizzBuzzGenerator.Generate(15, this.logger.Object);
             result.Should().Be("FizzBuzz");
+            this.VerifyLogging("Item '15' is processed");
         }
+
+        [TestMethod]
+        public void GivenGenerateWhenNeitherMultiplesOfThreeOrFiveThenNoFizzAndOrBuzz()
+        {
+            var result = FizzBuzzGenerator.Generate(1, this.logger.Object);
+            result.Should().Be(string.Empty);
+            this.VerifyLogging("Item '1' is processed");
+        }
+
+        private void VerifyLogging(string message)
+        {
+            this.logger
+                .Verify(
+                    x => 
+                        x.Log(
+                            LogLevel.Information, 
+                            It.IsAny<EventId>(), 
+                            It.Is<FormattedLogValues>(y => y[0].Value.ToString() == message), 
+                            null, 
+                            It.IsAny<Func<object, Exception, string>>()), 
+                    Times.Once);
+        }
+
     }
 }
