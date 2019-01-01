64540aed8499280f3e06e80165c2b0aaa1bb1882
nick <nickthebish@hotmail.com>
2019-01-01 18:35:38 +0000

    Add queue instance test.

--- Haskell.FizzBuzzFunctions/Haskell.FizzBuzz.Client.Tests/FizzBuzzRegistryTests.cs
+++ Haskell.FizzBuzzFunctions/Haskell.FizzBuzz.Client.Tests/FizzBuzzRegistryTests.cs
@@ -12,18 +12,39 @@
         [TestMethod]
         public void IsFizzBuzzClientFound()
         {
-            Mock<IConfiguration> configuration = new Mock<IConfiguration>();
-            configuration.SetupGet(x => x["Connection"]).Returns("DefaultEndpointsProtocol=https;AccountName=storagequeuehaskell;AccountKey=IiSyYS3cs5sWCFPQhjQ4qqRJEsI3AFdwxpDeqmGn4idL+4iSsiVBTEQ7mPc+4CoLDT6t/f7dmOLDmXJMEJ4N9g==;BlobEndpoint=https://storagequeuehaskell.blob.core.windows.net/;QueueEndpoint=https://storagequeuehaskell.queue.core.windows.net/;TableEndpoint=https://storagequeuehaskell.table.core.windows.net/;FileEndpoint=https://storagequeuehaskell.file.core.windows.net/;");
-            configuration.SetupGet(x => x["InputQueueName"]).Returns("input");
-            configuration.SetupGet(x => x["OutputQueueName"]).Returns("output");
-
-            using (var container = new Container(new FizzBuzzRegistry(configuration.Object)))
+            using (var container = GetContainer(MockConfiguration()))
             {
                 container
                     .GetInstance<IFizzBuzzClient>()
                     .Should()
                     .BeOfType<FizzBuzzClient>();
             }
+        }
+
+        [TestMethod]
+        public void IsQueueFound()
+        {
+            using (var container = GetContainer(MockConfiguration()))
+            {
+                container
+                    .GetInstance<IQueue>()
+                    .Should()
+                    .BeOfType<Queue>();
+            }
+        }
+
+        private static Mock<IConfiguration> MockConfiguration()
+        {
+            Mock<IConfiguration> configuration = new Mock<IConfiguration>();
+            configuration.SetupGet(x => x["Connection"]).Returns("DefaultEndpointsProtocol=https;AccountName=storagequeuehaskell;AccountKey=IiSyYS3cs5sWCFPQhjQ4qqRJEsI3AFdwxpDeqmGn4idL+4iSsiVBTEQ7mPc+4CoLDT6t/f7dmOLDmXJMEJ4N9g==;BlobEndpoint=https://storagequeuehaskell.blob.core.windows.net/;QueueEndpoint=https://storagequeuehaskell.queue.core.windows.net/;TableEndpoint=https://storagequeuehaskell.table.core.windows.net/;FileEndpoint=https://storagequeuehaskell.file.core.windows.net/;");
+            configuration.SetupGet(x => x["InputQueueName"]).Returns("input");
+            configuration.SetupGet(x => x["OutputQueueName"]).Returns("output");
+            return configuration;
+        }
+
+        private static Container GetContainer(Mock<IConfiguration> configuration)
+        {
+            return new Container(new FizzBuzzRegistry(configuration.Object));
         }
     }
 }
